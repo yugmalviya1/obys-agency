d5df24be966dce31cd500ef47b4f9d9c310fc75f
yugmalviya1 <yugmalviya1@gmail.com>
2026-02-24 04:55:43 +0530

    page 1 done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function loadingAnimation(){
 var tl =  gsap.timeline()
 
 tl.from(".line h1",{             // slide up animation
@@ -28,25 +29,51 @@ tl.from("#line1-part1",{      // timer animation
 });
 
 tl.to(".line h2",{
-    animemationName:"anime",
-    opacity:1
+    animationName:"anime",
+    opacity:1,
 })
 
 tl.to("#loader",{         // loader remove 
     opacity:0,
     duration:0.2,
-    delay:4
+    delay:4,
 });
 
 tl.from("#page1",{
     delay:0.2,
     y:1600,
-    duration:0.5,
-    opacity:0,
-    ease:Power4
+    duration:0.6,
+    ease: Power4,
 });
 
 tl.to("#loader",{
-    display:"none"
-})
+    display:"none",
+});
+
+tl.from("#nav",{
+    opacity:0,
+    stagger:0.2,
+});
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2,
+});
+
+
+};
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4", {}); //Magnet Effect
+};
+
+loadingAnimation();
+cursorAnimation();
 
